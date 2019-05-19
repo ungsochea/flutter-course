e5c78ff24f92071ee0e8e01a4bab632db2ece7dd
ungsochea <sochea.ung@gmail.com>
2019-05-19 17:47:14 +0700

    2. Diving Into the Basics & Understanding Widgets

---
+++ lib/product_manager.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct = 'Sweet Tester Defualt'});
+
+  @override
+  _ProductManagerState createState() => _ProductManagerState();
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product){
+     setState(() {
+        _products.add(product);
+        // print(_products);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
