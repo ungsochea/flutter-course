ed67d247f04a7a736ad9e4b527efd9031ec1b95b
ungsochea <sochea.ung@gmail.com>
2019-05-21 09:40:02 +0700

    pass date to page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title,this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: Text('Product Detail'),
-          ),
-          body: Column(children: <Widget>[
-            Text('Detail!'),
-            RaisedButton(child: Text('BACK'),onPressed: () => Navigator.pop(context),)
-          ],),
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                child: Text('BACK'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,7 +4,7 @@ import './products.dart';
 import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
-  final String startingProduct;
+  final Map<String,String> startingProduct;
 
   ProductManager({this.startingProduct});
 
@@ -13,7 +13,7 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = [];
+  List<Map<String,String>> _products = [];
 
   @override
   void initState() {
@@ -25,11 +25,10 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override 
   void didUpdateWidget(ProductManager oldWidget) {
-    // TODO: implement didUpdateWidget
     super.didUpdateWidget(oldWidget);
   }
 
-  void _addProduct(String product){
+  void _addProduct(Map<String,String> product){
      setState(() {
         _products.add(product);
         // print(_products);
