adc1cc76fe2f7aaf834a7db53267bad84a856554
ungsochea <sochea.ung@gmail.com>
2019-05-21 15:37:08 +0700

    untrack route

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,22 @@ import './pages/product.dart';
 
 main() => runApp(MyApp());
 
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String,String>> _products = [];
+  List<Map<String, String>> _products = [];
 
-  void _addProduct(Map<String,String> product){
-     setState(() {
-        _products.add(product);
-        // print(_products);
-      });
+  void _addProduct(Map<String, String> product) {
+    setState(() {
+      _products.add(product);
+      // print(_products);
+    });
   }
 
-  void _deleteProduct(int index){
+  void _deleteProduct(int index) {
     setState(() {
       _products.removeAt(index);
     });
@@ -38,20 +37,27 @@ class _MyAppState extends State<MyApp> {
           accentColor: Colors.deepPurple),
       // home: AuthPage(),
       routes: {
-        '/':(BuildContext context) => ProductsPage(_products,_addProduct,_deleteProduct),
-        '/admin':(BuildContext context) => ProductAdminPage(),
+        '/': (BuildContext context) =>
+            ProductsPage(_products, _addProduct, _deleteProduct),
+        '/admin': (BuildContext context) => ProductAdminPage(),
       },
-      onGenerateRoute: (RouteSettings setting){
+      onGenerateRoute: (RouteSettings setting) {
         final List<String> pathElements = setting.name.split('/');
-        if(pathElements[0] != ''){
+        if (pathElements[0] != '') {
           return null;
         }
-        if(pathElements[1] == 'product'){
+        if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(builder: (BuildContext context) => ProductPage(
-            _products[index]['title'],_products[index]['image']
-          ));
+          return MaterialPageRoute<bool>(
+              builder: (BuildContext context) => ProductPage(
+                  _products[index]['title'], _products[index]['image']));
         }
+        return null;
+      },
+      onUnknownRoute: (RouteSettings setting) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                ProductsPage(_products, _addProduct, _deleteProduct));
       },
     );
   }
