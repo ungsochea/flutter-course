fcb5b71dc07bd86871787f928678728bd36d901b
ungsochea <sochea.ung@gmail.com>
2019-05-21 17:22:53 +0700

    input field with button

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  _ProductCreatePageState createState() => _ProductCreatePageState();
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: (){
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context){
-              return Center(child: Text('This is a Modal !'),);
-            }
-          );
-        },
-        )
-      
-    ,);
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Description'),
+            maxLength: 4,
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+         RaisedButton(child: Text('Save'),onPressed: (){},)
+        ],
+      ),
+    );
   }
 }
