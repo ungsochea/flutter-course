bf21037a33fb01dcecc9f3b5b431eae6dae56b2a
ungsochea <sochea.ung@gmail.com>
2019-05-21 09:11:32 +0700

    multi pages and navigator, route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './product_manager.dart';
+import './pages/home.dart';
 
 main() => runApp(MyApp());
 
@@ -9,15 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: ProductManager()),
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
     );
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
   final List<String> products;
 
   Products([this.products = const []]);
 
-  Widget _buildProductItem(BuildContext context,int index){
+  Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text(products[index])
-                    ],
-                  ),
-                );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage())),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
-  Widget _buildProductList(){
+  Widget _buildProductList() {
     Widget productCard;
-    if(products.length > 0){
+    if (products.length > 0) {
       productCard = ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    );
-    }else{
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
       // productCard = Center(child: Text('No product found, please add some'),);
       productCard = Container();
     }
